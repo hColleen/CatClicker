71d639e322c325da69be00d0cd30de36396b6656
hColleen <hcolleen@gmail.com>
2018-07-17 03:40:05 -0700

    rows working, clicking not yet

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-let catClicker = document.getElementById('clicker');
-let counter = document.getElementById('counter');
-count = 0;
+document.addEventListener("DOMContentLoaded", function(){
 
-catClicker.addEventListener('click', function(){
-	count += 1;
-	counter.innerHTML = `You have clicked ${count} times so far`;
-});
+	function Cat(number, name, count, image){
+		this.number = number;
+		this.name = name;
+		this.count = count;
+		this.image = image;
+		this.text = `<div id = 'cat${this.number}'><h2>This is ${this.name}</h2><img src = ${this.image}><p>You have clicked ${this.count} times so far.</p></div>`;
+	};
 	
+	let catOne = new Cat ("One", "Kagami", 0, "images/kagami.jpg"),
+		catTwo = new Cat ("Two", "Makai Neko", 0, "images/makai-neko.jpg");
+		
+	let row = document.getElementById('catHolder');
+	
+	row.innerHTML = catOne.text + catTwo.text;
+	
+	document.getElementsByTagName('IMG').addEventListener('click', function(){
+		this.count +=1;
+	});
+});
