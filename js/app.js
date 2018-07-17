d9977f4b61b930d727b752681d182db56800558a
hColleen <hcolleen@gmail.com>
2018-07-17 04:09:39 -0700

    update constructor function semamtics

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-	function Cat(number, name, count, image){
+	var Cat = function(number, name, count, image){
 		this.number = number;
 		this.name = name;
 		this.count = count;
 		this.image = image;
 		this.text = `<div id = 'cat${this.number}'><h2>This is ${this.name}</h2><img src = ${this.image}><p>You have clicked ${this.count} times so far.</p></div>`;
+	};
+	
+	Cat.prototype.click = function(){
+		this.image.addEventListener('click', function(){
+			this.count +=1;
+		});
 	};
 	
 	let catOne = new Cat ("One", "Kagami", 0, "images/kagami.jpg"),
@@ -14,8 +20,5 @@ document.addEventListener("DOMContentLoaded", function(){
 	let row = document.getElementById('catHolder');
 	
 	row.innerHTML = catOne.text + catTwo.text;
-	
-	document.getElementsByTagName('IMG').addEventListener('click', function(){
-		this.count +=1;
-	});
+
 });
